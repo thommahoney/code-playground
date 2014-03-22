50304ea4f368d8da5f1c222f27a6bdfe987cc7bd
Matthew O'Brien <matthew.obrien@theguardian.com>
2014-03-22 22:52:54 +0000

    Removed unused libraries. Upgraded the rest.

--- project/ApplicationBuild.scala
+++ project/ApplicationBuild.scala
@@ -5,7 +5,7 @@ object ApplicationBuild extends Build {
 
   lazy val defaultSettings = Defaults.defaultSettings ++ Seq(
     version := "1.0",
-    scalaVersion := "2.10.3",
+    scalaVersion := "2.10.4",
     scalacOptions := Seq(
       "-feature",
       "-language:implicitConversions",
@@ -19,15 +19,10 @@ object ApplicationBuild extends Build {
   )
 
   val dependencies = Seq(
-    "net.databinder.dispatch" %% "dispatch-core" % "0.9.4",
-    "net.liftweb" %% "lift-json" % "2.5.1",
-    "joda-time" % "joda-time" % "2.0",
-    "org.joda" % "joda-convert" % "1.2",
-    "com.gu" %% "fastly-api-client" % "0.1.3",
-    "commons-io" % "commons-io" % "2.1",
-    "commons-lang" % "commons-lang" % "2.5",
-    "commons-codec" % "commons-codec" % "1.9",
-    "org.scalatest" %% "scalatest" % "1.9.1" % "test"
+    "commons-lang" % "commons-lang" % "2.6",
+    "joda-time" % "joda-time" % "2.3",
+    "org.joda" % "joda-convert" % "1.6",
+    "commons-codec" % "commons-codec" % "1.9"
   )
   
   lazy val root = Project(id = "CodePlayground",
