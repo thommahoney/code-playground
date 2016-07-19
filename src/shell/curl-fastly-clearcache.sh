5565eccaa7b1e2e8883b5c6af3705000471b8110
Thom Mahoney <mahoneyt@gmail.com>
2016-07-19 15:00:50 -0700

    use Fastly-Key instead of X-Fastly-Key

--- src/shell/curl-fastly-clearcache.sh
+++ src/shell/curl-fastly-clearcache.sh
@@ -2,4 +2,4 @@
 
 # $1 fastly API key
 # $2 url without the protocol
-curl -v -X POST -H "X-Fastly-Key: $1" https://api.fastly.com/purge/$2
+curl -v -X POST -H "Fastly-Key: $1" https://api.fastly.com/purge/$2
--- src/shell/curl-fastly-stats-api.sh
+++ src/shell/curl-fastly-stats-api.sh
@@ -2,7 +2,7 @@
 
 # curl, show Fastly usage
 # $1 full Url without protcol
-curl -v -H "Accept: application/json" -H "X-Fastly-Key: $1" https://api.fastly.com/stats/usage
+curl -v -H "Accept: application/json" -H "Fastly-Key: $1" https://api.fastly.com/stats/usage
 
 # with proxy
-#curl -v -x https://proxy:3128 -H "Accept: application/json" -H "X-Fastly-Key: $1" https://api.fastly.com/stats/usage
+#curl -v -x https://proxy:3128 -H "Accept: application/json" -H "Fastly-Key: $1" https://api.fastly.com/stats/usage
